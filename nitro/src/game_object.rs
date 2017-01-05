80edea581cc02471314f6eec603ff8ddc422b2a9
Jake Kiesel <Jake Kiesel>
2017-01-05 00:05:46 -0700

    Setup component execution ordering

--- nitro/src/game_object.rs
+++ nitro/src/game_object.rs
@@ -3,7 +3,7 @@ use texture::Texture;
 use transform::Transform;
 use component::Component;
 use component::Message;
-use std::collections::LinkedList;
+use std::collections::BTreeMap;
 use std::mem;
 use physics::nphysics2d::object::{RigidBody, RigidBodyHandle};
 use physics::nphysics2d::math::Matrix;
@@ -14,38 +14,52 @@ pub struct GameObject {
     pub texture: Texture,
     body: Option<RigidBodyHandle<f64>>,
     // When searching for a component associated with a GameObject you will need to search both of
-    // these vectors. At any given time either one of them could contain the Component you are
+    // these collections. At any given time either one of them could contain the Component you are
     // searching for. As messages are distributed to components they are migrated from
     // components to messaged_components and once the distribution of a message is complete
-    // the two vectors are swapped.
-    components: LinkedList<Box<Component>>, // These have not had a message sent (yet).
-    messaged_components: LinkedList<Box<Component>>, // These have had a message sent.
+    // the two collections are swapped.
+    components: BTreeMap<i32, Box<Component>>, // These have not had a message sent (yet).
+    messaged_components: BTreeMap<i32, Box<Component>>, // These have had a message sent.
 }
 
 impl GameObject {
     pub fn new(app: &mut App) -> GameObject {
         GameObject {
             transform: Transform::new(),
-            components: LinkedList::new(),
-            messaged_components: LinkedList::new(),
+            components: BTreeMap::new(),
+            messaged_components: BTreeMap::new(),
             texture: Texture::empty(app),
             body: None,
         }
     }
 
     pub fn update(&mut self, app: &mut App, delta_time: f64) {
-        let mut pop_result = self.components.pop_front();
-        while let Some(mut component) = pop_result {
-            component.receive_message(app, self, &Message::Update { delta_time: delta_time });
-            self.messaged_components.push_back(component);
-            pop_result = self.components.pop_front();
+        let message = &Message::Update { delta_time: delta_time };
+        for key in self.components.keys().map(|x| *x).collect::<Vec<i32>>() {
+            if let Some(mut component) = self.components.remove(&key) {
+                component.receive_message(app, self, &message);
+                self.messaged_components.insert(key, component);
+            }
         }
         assert_eq!(self.components.len(), 0);
         mem::swap(&mut self.components, &mut self.messaged_components);
     }
 
+    pub fn component_indices(&self) -> Vec<i32> {
+        self.components.keys().map(|x| *x).collect::<Vec<i32>>()
+    }
+
+    pub fn get_component(&self, index: i32) -> Option<&Box<Component>> {
+        self.components.get(&index)
+    }
+
+    pub fn add_component_at_index(&mut self, component: Box<Component>, index: i32) {
+        self.components.insert(index, component);
+    }
+
     pub fn add_component(&mut self, component: Box<Component>) {
-        self.components.push_front(component);
+        let new_key = self.components.keys().nth(0).unwrap_or(&1) - 1;
+        self.components.insert(new_key, component);
     }
 
     pub fn set_rigid_body(&mut self, app: &mut App, rigid_body: RigidBody<f64>) {
