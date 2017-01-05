76b5554b9ad9de48da68e17a2ff18fb74942621a
Jacob Kiesel <kieseljake@gmail.com>
2017-01-05 13:21:04 -0700

    Refactored components a bit

--- nitro/src/game_object.rs
+++ nitro/src/game_object.rs
@@ -4,18 +4,16 @@ use transform::Transform;
 use component::Component;
 use component::Message;
 use std::collections::BTreeMap;
-use std::any::Any;
 use std::any::TypeId;
 use physics::nphysics2d::object::{RigidBody, RigidBodyHandle};
 use physics::nphysics2d::math::Matrix;
-use physics::nalgebra::{Rotation2, Vector2, Vector1};
+use physics::nalgebra::{Rotation2, Vector2, Vector1, Rotation};
 
 pub struct GameObject {
     pub transform: Transform,
     pub texture: Texture,
-    body: Option<RigidBodyHandle<f64>>,
-    components: BTreeMap<i32, Box<Component>>,
-    component_types: BTreeMap<i32, TypeId>, // Identical to components just stores their type instead.
+    pub body: Option<RigidBodyHandle<f64>>,
+    components: BTreeMap<i32, (Box<Component>, TypeId)>,
 }
 
 impl GameObject {
@@ -23,7 +21,6 @@ impl GameObject {
         GameObject {
             transform: Transform::new(),
             components: BTreeMap::new(),
-            component_types: BTreeMap::new(),
             texture: Texture::empty(app),
             body: None,
         }
@@ -32,36 +29,41 @@ impl GameObject {
     pub fn update(&mut self, app: &mut App, delta_time: f64) {
         let message = &Message::Update { delta_time: delta_time };
         for key in self.components.keys().map(|x| *x).collect::<Vec<i32>>() {
-            if let Some(mut component) = self.components.remove(&key) {
+            if let Some((mut component, type_id)) = self.components.remove(&key) {
                 component.receive_message(app, self, &message);
-                self.components.insert(key, component);
+                self.components.insert(key, (component, type_id));
             }
         }
     }
 
-    pub fn component_indices(&self) -> Vec<i32> {
+    pub fn component_keys(&self) -> Vec<i32> {
         self.components.keys().map(|x| *x).collect::<Vec<i32>>()
     }
 
-    pub fn get_components_of_type<T>(&self) -> Vec<i32> where T: Component + 'static {
-        self.component_types.iter().filter_map(|(k, v)| {
-            if *v == TypeId::of::<T>() {Some(*k)}
+    pub fn component_keys_with_type<T>(&self) -> Vec<i32> where T: Component + 'static {
+        self.components.iter().filter_map(|(k, &(_, type_id))| {
+            if type_id == TypeId::of::<T>() {Some(*k)}
             else {None}
         }).collect::<Vec<i32>>()
     }
 
-    pub fn get_component(&mut self, index: i32) -> Option<&mut Box<Component>> {
+    pub fn component(&self, index: i32) -> Option<&(Box<Component>, TypeId)> {
+        self.components.get(&index)
+    }
+
+    pub fn component_mut(&mut self, index: i32) -> Option<&mut (Box<Component>, TypeId)> {
         self.components.get_mut(&index)
     }
 
-    pub fn add_component_at_index(&mut self, component: Box<Component>, index: i32) {
-        self.components.insert(index, component);
+    pub fn insert_component<T>(&mut self, component: T, index: i32)
+    -> Option<(Box<Component + 'static>, TypeId)>
+    where T: Component + 'static {
+        self.components.insert(index, (Box::new(component), TypeId::of::<T>()))
     }
 
     pub fn add_component<T>(&mut self, component: T) -> i32 where T: Component + 'static {
-        let new_key = self.components.keys().nth(0).unwrap_or(&1) - 1;
-        self.component_types.insert(new_key, TypeId::of::<T>());
-        self.components.insert(new_key, Box::new(component));
+        let new_key = self.components.keys().map(|x| *x).nth(0).unwrap_or(1) - 1;
+        self.components.insert(new_key, (Box::new(component), TypeId::of::<T>()));
         new_key
     }
 
@@ -75,10 +77,7 @@ pub fn copy_from_physics(game_object: &mut GameObject) {
         let body_borrow = body_box.borrow();
         *game_object.transform.mut_x() = body_borrow.position().translation.x;
         *game_object.transform.mut_y() = body_borrow.position().translation.y;
-        let matrix = body_borrow.position().rotation.submatrix();
-        let y = matrix.m12;
-        let x = matrix.m11;
-        *game_object.transform.mut_rotation() = y.atan2(x);
+        *game_object.transform.mut_rotation() = body_borrow.position().rotation.rotation().x
     }
 }
 
