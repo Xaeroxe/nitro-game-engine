fa25340f6a6ba01fa62d42f8f1cc89f8c2084c49
Jacob Kiesel <kieseljake@gmail.com>
2017-02-28 13:47:07 -0700

    Configure windows build.  Builds but won't run atm

---
+++ build.rs
@@ -0,0 +1,51 @@
+use std::env;
+use std::path::{PathBuf, Path};
+
+fn main() {
+    let target = env::var("TARGET").unwrap();
+    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let mut lib_dir = manifest_dir.clone();
+    let mut dll_dir = manifest_dir.clone();
+    if target == "x86_64-pc-windows-msvc" {
+        lib_dir.push("msvc");
+        lib_dir.push("lib");
+        lib_dir.push("64");
+        dll_dir.push("msvc");
+        dll_dir.push("dll");
+        dll_dir.push("64");
+    }
+    else if target == "x86_64-pc-windows-gnu" {
+        lib_dir.push("gnu");
+        lib_dir.push("lib");
+        lib_dir.push("64");
+        dll_dir.push("gnu");
+        dll_dir.push("dll");
+        dll_dir.push("64");
+    }
+    else if target == "i686-pc-windows-msvc" {
+        lib_dir.push("msvc");
+        lib_dir.push("lib");
+        lib_dir.push("32");
+        dll_dir.push("msvc");
+        dll_dir.push("dll");
+        dll_dir.push("32");
+    }
+    else if target == "i686-pc-windows-gnu" {
+        lib_dir.push("gnu");
+        lib_dir.push("lib");
+        lib_dir.push("32");
+        dll_dir.push("gnu");
+        dll_dir.push("dll");
+        dll_dir.push("32");
+    }
+    println!("cargo:rustc-link-search=static={}", lib_dir.display());
+    for entry in std::fs::read_dir(dll_dir).expect("Can't read DLL dir")  {
+        let entry_path = entry.expect("Invalid fs entry").path();
+        let file_name_result = entry_path.file_name();
+        let mut new_file_path = manifest_dir.clone();
+        if let Some(file_name) = file_name_result {
+            new_file_path.push(file_name.to_str().unwrap());
+            std::fs::copy(&entry_path, new_file_path.as_path()).expect("Can't copy from DLL dir");
+        }
+    }
+}
