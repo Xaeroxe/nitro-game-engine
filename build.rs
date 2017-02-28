718cc156737233d2d1d54aaafdd4b34c10e976dc
Jacob Kiesel <kieseljake@gmail.com>
2017-02-28 13:50:03 -0700

    I broke other platforms with last commit. Fixed.

--- build.rs
+++ build.rs
@@ -38,14 +38,16 @@ fn main() {
         dll_dir.push("dll");
         dll_dir.push("32");
     }
-    println!("cargo:rustc-link-search=static={}", lib_dir.display());
-    for entry in std::fs::read_dir(dll_dir).expect("Can't read DLL dir")  {
-        let entry_path = entry.expect("Invalid fs entry").path();
-        let file_name_result = entry_path.file_name();
-        let mut new_file_path = manifest_dir.clone();
-        if let Some(file_name) = file_name_result {
-            new_file_path.push(file_name.to_str().unwrap());
-            std::fs::copy(&entry_path, new_file_path.as_path()).expect("Can't copy from DLL dir");
+    if target.contains("pc-windows") {
+        println!("cargo:rustc-link-search=static={}", lib_dir.display());
+        for entry in std::fs::read_dir(dll_dir).expect("Can't read DLL dir")  {
+            let entry_path = entry.expect("Invalid fs entry").path();
+            let file_name_result = entry_path.file_name();
+            let mut new_file_path = manifest_dir.clone();
+            if let Some(file_name) = file_name_result {
+                new_file_path.push(file_name.to_str().unwrap());
+                std::fs::copy(&entry_path, new_file_path.as_path()).expect("Can't copy from DLL dir");
+            }
         }
     }
 }
